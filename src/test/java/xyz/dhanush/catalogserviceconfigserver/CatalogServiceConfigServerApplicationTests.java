b9eaf4db56b75de115f29902e0e85be405fb0a79
b1ackshadow <actuallydc@gmail.com>
2024-11-17 09:20:59 +0530

    Config server setup

---
+++ src/test/java/xyz/dhanush/catalogserviceconfigserver/CatalogServiceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.dhanush.catalogserviceconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
